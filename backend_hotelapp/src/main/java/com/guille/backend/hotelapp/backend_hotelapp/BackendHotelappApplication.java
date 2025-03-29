ab67a6d6b85672fc0cb4e4cb5358e5a07b9aee89
Guillago05 <guillermogc02@gmail.com>
2025-03-29 18:41:55 +0100

    Backend creado

---
+++ backend_hotelapp/src/main/java/com/guille/backend/hotelapp/backend_hotelapp/BackendHotelappApplication.java
@@ -0,0 +1,13 @@
+package com.guille.backend.hotelapp.backend_hotelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHotelappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHotelappApplication.class, args);
+	}
+
+}
---
+++ backend_hotelapp/src/test/java/com/guille/backend/hotelapp/backend_hotelapp/BackendHotelappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guille.backend.hotelapp.backend_hotelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHotelappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
